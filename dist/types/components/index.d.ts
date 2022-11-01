fe41d9b63eb673fb57cfd90098dddeae82beb835
uv43kv <marius-florin.patrascu@ing.com>
2022-11-01 13:17:13 +0200

    remove dist folder
    Signed-off-by: uv43kv <marius-florin.patrascu@ing.com>

--- dist/types/components/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { EgeriaLogin } from './Login';
-import { RequireAuth } from './RequireAuth';
-import { EgeriaAbout } from './About';
-import { EgeriaHome, links } from './Home';
-import { EgeriaApp } from './App';
-export { EgeriaAbout, EgeriaApp, EgeriaHome, EgeriaLogin, RequireAuth, links };
