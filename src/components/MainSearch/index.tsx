8501e667a794905968a264015e12951bd7aa5c2e
uv43kv <marius-florin.patrascu@ing.com>
2022-11-02 11:57:09 +0200

    added validation for main search
    Signed-off-by: uv43kv <marius-florin.patrascu@ing.com>

--- src/components/MainSearch/index.tsx
+++ src/components/MainSearch/index.tsx
@@ -1,8 +1,8 @@
-import { fetchTypes, ASSET_CATALOG_PATH } from "@lfai/egeria-js-commons";
-import { Loader, MultiSelect, TextInput } from "@mantine/core";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Search } from "tabler-icons-react";
+import {fetchTypes, ASSET_CATALOG_PATH, isStringLonger, QUERY_MIN_LENGTH, isArrayEmpty} from "@lfai/egeria-js-commons";
+import {Loader, MultiSelect, TextInput} from "@mantine/core";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Search} from "tabler-icons-react";
 
 export function MainSearch() {
   const navigate = useNavigate();
@@ -12,12 +12,12 @@ export function MainSearch() {
     typesData: []
   });
 
-  const [types, setTypes]: [any, any] = useState([]);
-  const [q, setQ]: [any, any] = useState('');
+  const [types, setTypes]: [any, any] = useState({value: [], isPristine: true});
+  const [q, setQ]: [any, any] = useState({value: '', isPristine: true});
 
   const handleKeyPress = (event: any) => {
-    if(event.key === 'Enter'){
-      navigate(`${ASSET_CATALOG_PATH}?q=${q}&types=${types.join(',')}`)
+    if (event.key === 'Enter') {
+      navigate(`${ASSET_CATALOG_PATH}?q=${q.value}&types=${types.value.join(',')}`)
     }
   };
 
@@ -38,26 +38,28 @@ export function MainSearch() {
 
   return <>
     <MultiSelect data={typesData.typesData}
-                  value={types}
-                  disabled={typesData.isLoading}
-                  onChange={(value: any) => setTypes([...value])}
-                  radius="sm"
-                  size="xs"
-                  placeholder="Type"
-                  style={{width:'15%', marginRight: '1%'}}
-                  rightSection={
-                    typesData.isLoading ? <Loader size={22} /> : <></>
-                  } />
+                 value={types.value}
+                 disabled={typesData.isLoading}
+                 onChange={(value: any) => setTypes({value: [...value]})}
+                 error={!types.isPristine && isArrayEmpty(types.value) ? 'At least one type has to be selected' : ''}
+                 radius="sm"
+                 size="xs"
+                 placeholder="Type"
+                 style={{width: '15%', marginRight: '1%'}}
+                 rightSection={
+                   typesData.isLoading ? <Loader size={22}/> : <></>
+                 }/>
 
-    <TextInput style={{width:'30%'}}
-                icon={<Search size={18} />}
-                radius="sm"
-                size="xs"
-                value={q}
-                disabled={typesData.isLoading}
-                onKeyPress={handleKeyPress}
-                onChange={(event: any) => setQ(event.currentTarget.value)}
-                placeholder="Search terms"
-                rightSectionWidth={42} />
+    <TextInput style={{width: '30%'}}
+               icon={<Search size={18}/>}
+               radius="sm"
+               size="xs"
+               value={q.value}
+               disabled={typesData.isLoading}
+               error={!q.isPristine && !isStringLonger(q.value, QUERY_MIN_LENGTH) ? 'Query must be at least ' + QUERY_MIN_LENGTH + ' characters' : ''}
+               onKeyPress={handleKeyPress}
+               onChange={(event: any) => setQ({value: event.currentTarget.value})}
+               placeholder="Search terms"
+               rightSectionWidth={42}/>
   </>
 }
