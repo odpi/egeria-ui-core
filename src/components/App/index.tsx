743bd7c105b83a8ea48b2f0cd2136012d40ab5db
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2023-04-13 11:59:31 +0300

    Improvements on authentication and login logout situations
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,9 @@
-import {
-  AppShell,
-  useMantineTheme
-} from '@mantine/core';
-import {NotificationsProvider, showNotification} from '@mantine/notifications';
-import {useEffect} from 'react';
-
-import {EgeriaHeader} from '../Header';
-import {EgeriaNavbar} from '../NavbarMinimal';
+import { AppShell, useMantineTheme } from '@mantine/core';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { NotificationsProvider, showNotification } from '@mantine/notifications';
+import { EgeriaHeader } from '../Header';
+import { EgeriaNavbar } from '../NavbarMinimal';
 
 interface Props {
   main?: React.ReactNode;
@@ -26,12 +23,33 @@ const listenForAPIErrors = (e: any) => {
 export function EgeriaApp(props: React.PropsWithChildren<Props>) {
   const {single, menu, main} = props;
   const theme = useMantineTheme();
+  const navigate = useNavigate();
+
+  const navigateTo = (e: any): void => {
+    console.log('navigateTo');
+
+    if(e.detail && e.detail.path) {
+      navigate(e.detail.path);
+    }
+  };
+
+  const egeriaEvents = (e: any): void => {
+    switch(e.detail.type) {
+      case 'EGERIA_NAVIGATE_TO':
+        navigateTo(e);
+        break;
+      default:
+        console.log(`EVENT ${e.detail.type} NOT MAPPED`);
+    }
+  };
 
   useEffect(() => {
     document.addEventListener('EGERIA_API_ERROR', listenForAPIErrors);
+    document.addEventListener('EGERIA_EVENTS', egeriaEvents);
 
     return function cleanup() {
       document.removeEventListener('EGERIA_API_ERROR', listenForAPIErrors);
+      document.removeEventListener('EGERIA_EVENTS', egeriaEvents);
     };
   }, []);
 
